e609df86f4bfcd270c6fb63565e033c6dee431e2
terrencekuo <tkuo@princeton.edu>
2015-05-05 01:10:42 -0400

    apps

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -95,7 +95,7 @@ STATICFILES_DIRS = (
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Enable Connection Pooling
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
